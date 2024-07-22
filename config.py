567042506fe1843b064364bb78d42567fbb02cef
X1 <bxjjjzid@gmail.com>
2024-07-22 23:05:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 import os
+import re
+from os import environ
 
+id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     TELEGRAM_API = os.environ.get("TELEGRAM_API")
     OWNER = os.environ.get("OWNER")
+    AUTH_CHANNEL = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('AUTH_CHANNEL', '-1002233071560').split()] # give channel id with seperate space. Ex : ('-10073828 -102782829 -1007282828')
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
     PASSWORD = os.environ.get("PASSWORD")
     DATABASE_URL = os.environ.get("DATABASE_URL")
